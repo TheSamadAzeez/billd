67530e8b3f8278ae8a92651dd648817f48ef758c
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-12-10 00:43:00 +0100

    feat: refactor layout component to include Sidebar and improve code organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import Sidebar from '@/components/custom/sidebar'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
-});
+})
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
-});
+})
 
 export const metadata: Metadata = {
   title: {
@@ -18,20 +19,18 @@ export const metadata: Metadata = {
     template: '%s | ACME',
   },
   description: 'A modern Next.js starter template with Shadcn UI and Biome.',
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className="light">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Sidebar>{children}</Sidebar>
       </body>
     </html>
-  );
+  )
 }
